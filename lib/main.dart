9f3764d61fd47a8579dfc6ef89c74de61ae0acd2
Kamronbek <115008454+Kamronbek477@users.noreply.github.com>
2024-05-04 00:40:28 +0500

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,66 +5,111 @@ void main() {
 }
 
 class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   State<MyApp> createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
+  late TextEditingController _firstController;
+  late TextEditingController _secondController;
+  String _firstText = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _firstController = TextEditingController();
+    _secondController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _firstController.dispose();
+    _secondController.dispose();
+    super.dispose();
+  }
+
+  int currentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          actions: [],
-          title: Text("6-mustaqil ish"),
-          backgroundColor: Colors.green,
+          title: Text("6-amaliy ish"),
+          backgroundColor: Colors.blue,
         ),
         body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             CircleAvatar(
               radius: 65,
-              backgroundImage: AssetImage("rasm/images.jpg"),
+              backgroundImage: AssetImage("rasm/1.jpg"),
             ),
             SizedBox(height: 12),
             TextField(
+              controller: _firstController,
               decoration: InputDecoration(
-                errorBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.green,
-                    width: 2,
-                  ),
-                ),
-                errorText: "Maydonni tuldiring",
+                filled: true,
+                fillColor: Colors.red,
+                hintText: "Matn kiriting✍...",
               ),
+              onChanged: (text) {
+                setState(() {
+                  _firstText = text;
+                });
+              },
             ),
             SizedBox(height: 12),
-            ElevatedButton(
-              onPressed: () {},
-              child: Text(
-                "Jo'natish",
+            TextField(
+              controller: _secondController,
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.green,
+                hintText: "takrorlanuvchi matn👆...",
               ),
+            ),
+            SizedBox(height: 30),
+            ElevatedButton(
+              onPressed: () {
+                setState(() {
+                  _secondController.text = _firstText;
+                });
+              },
+              child: Text("Yuborish👌"),
             )
           ],
         ),
         bottomNavigationBar: Theme(
-          data: ThemeData(canvasColor: Colors.yellow),
+          data: ThemeData(canvasColor: Colors.black54),
           child: BottomNavigationBar(
             backgroundColor: Colors.black54,
-            fixedColor: Colors.red,
+            iconSize: 24,
+            currentIndex: currentIndex,
+            onTap: (index) {
+              setState(() {
+                currentIndex = index;
+              });
+            },
             items: [
               BottomNavigationBarItem(
-                  icon: Icon(Icons.home_filled), label: "Home"),
+                icon: Icon(Icons.home_filled),
+                label: "Home",
+              ),
               BottomNavigationBarItem(
-                  icon: Icon(Icons.wallet), label: "Wallet"),
+                icon: Icon(Icons.wallet),
+                label: "Wallet",
+              ),
               BottomNavigationBarItem(
-                  icon: Icon(Icons.pie_chart), label: "Pie chart"),
+                icon: Icon(Icons.pie_chart),
+                label: "Pie chart",
+              ),
               BottomNavigationBarItem(
-                  icon: Icon(Icons.person), label: "Person"),
+                icon: Icon(Icons.person),
+                label: "Person",
+              ),
             ],
           ),
         ),
